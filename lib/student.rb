94c7dc8e1f83107afb6a281799d15bb8ca82b8b3
Nick Warren <nickwarren47@gmail.com>
2022-08-13 10:56:47 -0600

    commit tree 11a547c7be6cffcd996a1392514f57d31387b171

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
-class Student 
+class Student < User
+    attr_reader :knowledge
+
+    def initialize
+        @knowledge = []
+    end
+
+    def learn(knowledge) 
+        @knowledge << knowledge
+    end
+
+    # note: if we don't have the attr_reader, we would need this
+    # def knowledge
+    #     @knowledge
+    # end
 
 end
